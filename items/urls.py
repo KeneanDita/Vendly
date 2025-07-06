387e9562eecfaa38a0cb6b5f8221844408be29dd
Kenean Dita <KeneanSufa@gmail.com>
2025-07-06 21:07:16 +0300

    Adding new items works

--- items/urls.py
+++ items/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 app_name = "items"
+app_name = "items"
 
 
 urlpatterns = [
+    path('new/', views.new, name='new'),
     path("<int:pk>/", views.detail, name="detail"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
+from django.contrib.auth.decorators import login_required
+from .forms import NewItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=item.pk)[:4]
     
     return render(request, 'items/detail.html', {'item': item, 'related_items': related_items})
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid:
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'items/form.html', {
+        'form':form,
+        'title': 'New Item',
+    })
