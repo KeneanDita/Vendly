7a04217559e8e25c6353eb3cab8ad74d24232527
Kenean Dita <KeneanSufa@gmail.com>
2025-07-08 20:20:12 +0300

    filter done

--- items/views.py
+++ items/views.py
@@ -6,14 +6,20 @@ from .forms import NewItemForm, EditItemForm
 
 def items(request):
     query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0 )
     categories = Category.objects.all()
     items = Item.objects.filter(is_sold=False)
+    
+    if category_id:
+        items=items.filter(category_id=category_id)
+    
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
     return render(request, 'items/items.html', {
         'items':items,
         'query':query,
         'categories':categories,
+        'category_id':int(category_id),
     })
 
 def detail(request, pk):
