b8c8ef1ba1bc54b48b9387f9c35480970ea25202
Kenean Dita <KeneanSufa@gmail.com>
2025-07-08 11:29:17 +0300

    Delete your own items complete

--- items/urls.py
+++ items/urls.py
@@ -8,4 +8,5 @@ app_name = "items"
 urlpatterns = [
     path('new/', views.new, name='new'),
     path("<int:pk>/", views.detail, name="detail"),
+    path("int:pk>/delete/", views.delete, name='delete'),
 ]
